315f043f857ea9062ac56413e6c97f4b268cf34e
orsond <orsondmc@gmail.com>
2021-08-31 11:03:50 +1000

    NEW: user new plugin loader

---
+++ src/main/java/com/collarmc/example/forgemod/ExampleCollarPlugin.java
@@ -0,0 +1,52 @@
+package com.collarmc.example.forgemod;
+
+import com.collarmc.client.api.messaging.events.PrivateMessageReceivedEvent;
+import com.collarmc.client.api.plugin.CollarPlugin;
+import com.collarmc.client.api.plugin.CollarPluginLoadedEvent;
+import com.collarmc.client.events.CollarStateChangedEvent;
+import com.collarmc.pounce.EventBus;
+import com.collarmc.pounce.Preference;
+import com.collarmc.pounce.Subscribe;
+
+/**
+ * Add to entry points in collar.mod.json
+ *     "collar": [
+ *       "com.collarmc.example.fabricmod.client.ExampleCollarPlugin"
+ *     ]
+ */
+public class ExampleCollarPlugin implements CollarPlugin {
+
+    public static EventBus EVENT_BUS;
+
+    @Override
+    public void onLoad(CollarPluginLoadedEvent event) {
+        EVENT_BUS = event.eventBus;
+        // EVENT_BUS.subscribe(yourListener) here to receive Collar events
+    }
+
+    /**
+     * Listen to collar state changes
+     * @param event state change event
+     */
+    @Subscribe
+    public void onConnecting(CollarStateChangedEvent event) {
+        switch (event.state) {
+            case CONNECTING:
+            case CONNECTED:
+            case DISCONNECTING:
+            case DISCONNECTED:
+            default:
+                System.out.println("Collar is " + event.state);
+        }
+    }
+
+    /**
+     * Example that listens to {@link PrivateMessageReceivedEvent}
+     * Use {@link Preference} to decide what thread your event runs on
+     * @param e event
+     */
+    @Subscribe(Preference.CALLER)
+    public void onMessageReceived(PrivateMessageReceivedEvent e) {
+        System.out.println("Message received");
+    }
+}
--- src/main/java/com/collarmc/example/forgemod/ForgeMod.java
+++ src/main/java/com/collarmc/example/forgemod/ForgeMod.java
@@ -1,13 +1,5 @@
 package com.collarmc.example.forgemod;
 
-import com.collarmc.api.CollarPlugin;
-import com.collarmc.api.CollarPluginLoadedEvent;
-import com.collarmc.client.Collar;
-import com.collarmc.client.api.messaging.events.PrivateMessageReceivedEvent;
-import com.collarmc.client.events.CollarStateChangedEvent;
-import com.collarmc.pounce.EventBus;
-import com.collarmc.pounce.Preference;
-import com.collarmc.pounce.Subscribe;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod(
@@ -15,43 +7,9 @@ import net.minecraftforge.fml.common.Mod;
         name = ForgeMod.MOD_NAME,
         version = ForgeMod.VERSION
 )
-public class ForgeMod implements CollarPlugin {
+public class ForgeMod {
 
     public static final String MOD_ID = "forge-mod";
     public static final String MOD_NAME = "Collar Example Forge Mod";
     public static final String VERSION = "1.0-SNAPSHOT";
-
-    public static EventBus EVENT_BUS;
-
-    @Override
-    public void onLoad(CollarPluginLoadedEvent event) {
-        EVENT_BUS = event.eventBus;
-        // EVENT_BUS.subscribe(yourListener) here to receive Collar events
-    }
-
-    /**
-     * Listen to collar state changes
-     * @param event state change event
-     */
-    @Subscribe
-    public void onConnecting(CollarStateChangedEvent event) {
-        switch (event.state) {
-            case CONNECTING:
-            case CONNECTED:
-            case DISCONNECTING:
-            case DISCONNECTED:
-            default:
-                System.out.println("Collar is " + event.state);
-        }
-    }
-
-    /**
-     * Example that listens to {@link PrivateMessageReceivedEvent}
-     * Use {@link Preference} to decide what thread your event runs on
-     * @param e event
-     */
-    @Subscribe(Preference.CALLER)
-    public void onMessageReceived(PrivateMessageReceivedEvent e) {
-        System.out.println("Message received");
-    }
 }
